84d9505a960c8f50ac375a1089f9a6b9d9d5ea79
Rachel Clark <rachelbclark3@gmail.com>
2018-09-22 15:08:22 -0500

    Compiler error in Main Menu manager

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -59,5 +59,5 @@ public class MainMenuManager : MonoBehaviour
 	{
 
 	}
-}
+}*/
 
