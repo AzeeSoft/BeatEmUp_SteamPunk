54bc8fe8c42aa348742ef3fea923ce29f87394a4
Azee <aziztitu1996@gmail.com>
2018-09-23 14:02:08 -0500

    Implements Benjamin

---
+++ Assets/Scripts/Character/Benjamin/BenjaminCombatController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BenjaminCombatController : CharacterCombatController
+{
+    public GameObject shieldBurstPrefab;
+    public Transform shieldBurstSpawnTransform;
+
+    public ParticleSystem spiritChargingParticleSystem;
+    public GameObject explosionPrefab;
+
+    public override bool Block()
+    {
+        return true;
+    }
+
+    public override void Unblock()
+    {
+    }
+
+    public override bool LightAttack()
+    {
+        GameObject shieldBurstGameObject =
+            Instantiate(shieldBurstPrefab, shieldBurstSpawnTransform.position, shieldBurstSpawnTransform.rotation);
+
+        ShieldBurst shieldBurst = shieldBurstGameObject.GetComponent<ShieldBurst>();
+        shieldBurst.owner = _characterModel;
+
+        return true;
+    }
+
+    public override bool StartingSpiritCharge()
+    {
+        spiritChargingParticleSystem.Play();
+        return true;
+    }
+
+    public override void CancellingSpiritCharge()
+    {
+        spiritChargingParticleSystem.Stop();
+    }
+
+    public override void SpecialAttack(float chargedSpirit)
+    {
+        Debug.Log("Charged Spirit: " + chargedSpirit);
+        GameObject explosionGameObject = Instantiate(explosionPrefab, transform.position, transform.rotation);
+        explosionGameObject.GetComponent<Explosion>().Explode(_characterModel, chargedSpirit);
+    }
+}
---
+++ Assets/Scripts/Character/Benjamin/ShieldBurst.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldBurst : MonoBehaviour
+{
+    public float speed = 5;
+    public float maxLifetime = 2f;
+
+    public float damage = 5f;
+
+    public float volume = 1f;
+
+    [HideInInspector]
+    public CharacterModel owner;
+
+    private float startTime;
+    private ParticleSystem particleSystem;
+    private Light light;
+
+    void Awake()
+    {
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        startTime = Time.time;
+
+        if (volume > 0)
+        {
+            AudioManager.instance.PlayEffect(AudioManager.AudioData.TimeLightATK, transform, volume);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveShield();
+
+        BurnOut();
+    }
+
+    void MoveShield()
+    {
+        Vector3 finalPos = transform.position + (transform.forward * speed);
+        transform.position = Vector3.Lerp(transform.position, finalPos, Time.deltaTime);
+    }
+
+    void BurnOut()
+    {
+        if (Time.time - startTime > maxLifetime)
+        {
+            if (particleSystem.transform.localScale.magnitude > 0.05f)
+            {
+                particleSystem.transform.localScale =
+                    Vector3.Lerp(particleSystem.transform.localScale, Vector3.zero, Time.deltaTime);
+//                light.range = Mathf.Lerp(light.range, 0, Time.deltaTime);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnContactWithObject(GameObject otherGameObject)
+    {
+        if (owner == null || otherGameObject == owner.gameObject)
+        {
+            return;
+        }
+
+        CharacterHealthController characterHealthController =
+            otherGameObject.GetComponent<CharacterHealthController>();
+        if (characterHealthController != null)
+        {
+            characterHealthController.TakeDamage(damage, owner);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider otherCollider)
+    {
+        if (!otherCollider.isTrigger)
+        {
+            OnContactWithObject(otherCollider.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.collider.isTrigger)
+        {
+            OnContactWithObject(collision.gameObject);
+        }
+    }
+}
